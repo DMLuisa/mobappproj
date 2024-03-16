a4ea87490f7a5b1fb2a93f71384a84c2315b0320
DMLuisa <deaconumaria20@stud.ase.ro>
2024-03-16 17:52:21 +0200

    Models added

---
+++ Models/Books.cs
@@ -0,0 +1,41 @@
+﻿using project1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1.Models
+{
+    public enum Genre
+    {
+        Academic, Fantasy, Romance, Action, Novel, SciFi, Science, Mecha, Interstellar,
+        Research, Detective, Police, Humor, Horror, Report, Law, Guides, Thriller,
+        Math, Physics, Chemistry, Biology, Geography, Fhilosophy, History,
+        Astronomy, Archeology, Anthropology, Psychology, Medecine, 
+        Pharmachology, Agricultre, Engineering, Art, Sculpture, Sport,
+        Misc_Academic, Misc_Literature
+    };
+
+
+    public class Books
+    {
+        public string bookTitle { get; set; }
+        public string bookAuthor { get; set; }
+        public string bookDescription { get; set; }
+
+        public int noPhysCopies {  get; set; }
+
+        public Genre[] bookGenre { get; set; }
+
+        public Books()
+        {
+            bookTitle = "";
+            bookAuthor = "";
+            bookDescription = "";
+            noPhysCopies = 0;
+            bookGenre = new Genre[0];
+        }
+
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1.Models
+{
+    public class Users
+    {
+        public string firstName {  get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public bool isLibrarian { get; set; }
+
+        public Users() 
+        {
+            firstName = "";
+            lastName = "";
+            email = "";
+            password = "";
+            isLibrarian = false;
+        }
+    }
+}
